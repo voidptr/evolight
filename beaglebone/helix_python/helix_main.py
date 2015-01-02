57ae8488214bce12c0778e71bc0462724ec8e264
Rosangela Canino-Koning <voidptr@voidptr.net>
2015-01-01 23:21:55 -0400

    fixes

--- beaglebone/helix_python/helix_main.py
+++ beaglebone/helix_python/helix_main.py
@@ -72,14 +72,14 @@ def send_wholepop():
                 locus_offset = 0
 
         #fracval = 100.0
-        fracval = 1
+        fracval = 2
         for frac in range(int(fracval)):
             if len(old_lights) == 0:
                 interlights = lights
             else:
                 interlights = fade_intermediary(old_lights, lights, frac/fracval)
             conn.send_lights([interlights], [0]);
-            time.sleep(1)
+            time.sleep(.5)
         print "."
 
 
